1dbe4f9dc1011c9065f0dd91aa8ae1752e505a1c
msmartinez12 <msmartinez12@utpl.edu.ec>
2022-07-12 19:38:31 -0500

    cambios-flask

---
+++ taller/aplicacion-flask/app.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template
+import requests
+import json
+from config import user, password
+app = Flask(__name__, template_folder='templates')
+
+@app.route("/")
+def hello_world():
+    return "<p>Hola mundo, si se pudo!</p>"
+
+
+@app.route("/losedificios")
+def los_edificios():
+    r = requests.get("http://127.0.0.1:8000/api/edificios/",
+            auth=(user, password))
+    edificios = json.loads(r.content)['results']
+    numero_edificios = json.loads(r.content)['count']
+    return render_template("losedificios.html", edificios=edificios,
+    numero_edificios=numero_edificios)
+
+
+@app.route("/losdepartamentos")
+def los_departamentos():
+    """
+    """
+    r = requests.get("http://127.0.0.1:8000/api/departamentos/",
+            auth=(user, password))
+    datos = json.loads(r.content)['results']
+    numero = json.loads(r.content)['count']
+    return render_template("losdepartamentos.html", datos=datos,
+    numero=numero)
+
+
+@app.route("/losdepartamentosdos")
+def los_departamentos_dos():
+    """
+    """
+    r = requests.get("http://127.0.0.1:8000/api/departamentos/",
+            auth=(user, password))
+    datos = json.loads(r.content)['results']
+    numero = json.loads(r.content)['count']
+    datos2 = []
+    for d in datos:
+        datos2.append({'nombrePropietario':d['nombrePropietario'], 'costoDepartamento':d['costoDepartamento'],
+        'edificio': obtener_edificio(d['edificio'])})
+    return render_template("losdepartamentosdos.html", datos=datos2,
+    numero=numero)
+
+# funciones ayuda
+
+def obtener_edificio(url):
+    """
+    """
+    r = requests.get(url, auth=(user, password))
+    nombre_edificio = json.loads(r.content)['nombreEdificio']
+    return nombre_edificio
